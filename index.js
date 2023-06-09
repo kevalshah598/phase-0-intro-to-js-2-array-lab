50503a8826980e32980afa4da4abc314e6390b2e
Keval Shah <kevalshah598@gmail.com>
2023-06-09 00:36:43 -0400

    tests pass

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendCat(name){
+    cats.push(name);
+}
+
+function destructivelyPrependCat(name){
+    cats.unshift(name);
+}
+
+function destructivelyRemoveLastCat(){
+    cats.pop();
+}
+
+function destructivelyRemoveFirstCat(){
+    cats.shift();
+}
+
+function appendCat(name){
+    const newArray = cats.slice();
+    newArray.push(name);
+    return newArray;
+}
+function prependCat(name){
+    const newArray = cats.slice();
+    newArray.unshift(name);
+    return newArray;
+}
+
+function removeLastCat(name){
+    const newArray = cats.slice();
+    newArray.pop(name);
+    return newArray;
+}
+
+function removeFirstCat(name){
+    const newArray = cats.slice();
+    newArray.shift(name);
+    return newArray;
+}
